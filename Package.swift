1a045c7e4bfdb112e2b53d78855f0892e02646c3
Mac Long <hello@maclong.uk>
2024-06-02 11:38:30 +0100

    feat: added todo note for colors

--- Package.swift
+++ Package.swift
@@ -2,26 +2,26 @@
 import PackageDescription
 
 let package = Package(
-	name: "sls",
-	dependencies: [
-		.package(
-			url: "https://github.com/apple/swift-argument-parser",
-			from: "1.0.0"
-		),
-	],
-	targets: [
-		.executableTarget(
-			name: "sls",
-			dependencies: [
-				.product(
-					name: "ArgumentParser",
-					package: "swift-argument-parser"
-				),
-			]
-		),
-		.testTarget(
-			name: "SwiftListTests",
-			dependencies: ["sls"]
-		),
-	]
+  name: "sls",
+  dependencies: [
+    .package(
+      url: "https://github.com/apple/swift-argument-parser",
+      from: "1.0.0"
+    )
+  ],
+  targets: [
+    .executableTarget(
+      name: "sls",
+      dependencies: [
+        .product(
+          name: "ArgumentParser",
+          package: "swift-argument-parser"
+        )
+      ]
+    ),
+    .testTarget(
+      name: "SwiftListTests",
+      dependencies: ["sls"]
+    ),
+  ]
 )
--- Sources/SwiftList.swift
+++ Sources/SwiftList.swift
@@ -3,6 +3,17 @@ import Foundation
 
 let files = FileManager.default
 
+enum ConsoleColor: String {
+  case red = "\u{001B}[0;31m"
+  case blue = "\u{001B}[0;34m"
+  case reset = "\u{001B}[0;0m"
+}
+
+func determineColor(path: URL) -> String {
+  // TODO: Determine white, red and blue for file, executable and directory
+  return ""
+}
+
 func findContents(path: URL, _ all: Bool, _ recurse: Bool) throws -> String {
   var result = ""
   let contents = try files.contentsOfDirectory(
@@ -10,14 +21,15 @@ func findContents(path: URL, _ all: Bool, _ recurse: Bool) throws -> String {
     includingPropertiesForKeys: nil,
     options: all ? [] : [.skipsHiddenFiles]
   )
-  
+
   for url in contents {
-    result += "\(url.lastPathComponent)\t"
+    var color = determineColor(path: url)
+    result += "\(color)\(url.lastPathComponent)\(ConsoleColor.reset.rawValue)\t"
   }
-  
+
   if recurse {
     result += "\n"
-    
+
     for url in contents {
       if url.hasDirectoryPath {
         result += "\n./\(url.lastPathComponent):\n"
@@ -25,7 +37,7 @@ func findContents(path: URL, _ all: Bool, _ recurse: Bool) throws -> String {
       }
     }
   }
-  
+
   return result
 }
 
@@ -46,7 +58,7 @@ struct SwiftList: ParsableCommand {
     let files = FileManager.default
     let location = URL(fileURLWithPath: path ?? files.currentDirectoryPath)
     let result = try findContents(path: location, all, recurse)
-    
+
     print(result)
   }
 }
