3bd11733dd84e9f5b1af39b834833280f7d8c7fd
Mac Long <hello@maclong.uk>
2024-06-02 10:37:31 +0100

    fix: help message for path and default path value

--- Sources/SwiftList.swift
+++ Sources/SwiftList.swift
@@ -1,32 +1,32 @@
-import Foundation
 import ArgumentParser
+import Foundation
 
 let files = FileManager.default
 
 @main
 struct SwiftList: ParsableCommand {
-	@Flag(name: .shortAndLong, help: "Display all files, including hidden.")
-	var all = false
-	@Flag(name: .shortAndLong, help: "Display extended details and attributes.")
-	var long = false
-	@Flag(name: .shortAndLong, help: "Recurse into directories.")
-	var recurse = false
-	@Flag(name: .shortAndLong, help: "Colorize the output.")
-	var color = false
-	@Argument(help: "List the files at specified path.")
-	var path = files.currentDirectoryPath
-	
-	func run() throws {
-		print(path)
-		
-		do {
-			let contents = try files.contentsOfDirectory(atPath: path)
-			for file in contents {
-				print(file)
-			}
-		} catch {
-			print("Failed to list contents of directory: \(error)")
-		}
-	}
-}
+  @Flag(name: .shortAndLong, help: "Display all files, including hidden.")
+  var all = false
+  @Flag(name: .shortAndLong, help: "Display extended details and attributes.")
+  var long = false
+  @Flag(name: .shortAndLong, help: "Recurse into directories.")
+  var recurse = false
+  @Flag(name: .shortAndLong, help: "Colorize the output.")
+  var color = false
+  @Argument(help: "List files at path, omit for current directory.")
+  var path: String?
 
+  func run() throws {
+    do {
+      let contents = try files.contentsOfDirectory(
+		atPath: path ?? files.currentDirectoryPath
+      )
+		
+      for file in contents {
+        print(file)
+      }
+    } catch {
+      print("Failed to list contents of directory: \(error)")
+    }
+  }
+}
