b45d352c1add4c7820a41a004f631c7642cd5042
Mac Long <hello@maclong.uk>
2024-06-05 13:57:47 +0100

    feat: all flags completed

--- Sources/SwiftList.swift
+++ Sources/SwiftList.swift
@@ -1,8 +1,6 @@
 import ArgumentParser
 import Foundation
 
-let files = FileManager.default
-
 struct FileRepresentation {
   let icon: String
   let color: String
@@ -18,94 +16,129 @@ struct DisplayOptions {
   let oneLine: Bool
 }
 
-func getFileAttributes(_ location: URL, with opts: DisplayOptions) throws -> String {
-  let fileAttributes = try files.attributesOfItem(atPath: location.path)
-  let file = determineType(location)
+class FileManagerHelper {
+  static let fm = FileManager.default
 
-  return
-    "\(opts.icons ? "\(file.icon) " : "")\(opts.color ? file.color : "")\(opts.long ? fileAttributes[.posixPermissions] ?? "" : "") \(opts.long ? fileAttributes[.ownerAccountName] ?? "" : "") \(opts.long ? fileAttributes[.groupOwnerAccountName] ?? "" : "") \(opts.long ? fileAttributes[.size] ?? "" : "") \(opts.long ? fileAttributes[.modificationDate] ?? "" : "") \(location.lastPathComponent)\(opts.oneLine || opts.long ? "\n" : "  ")\(opts.color ? "\u{001B}[0;0m" : "")"
-}
-
-func determineType(_ location: URL) -> FileRepresentation {
-  if location.hasDirectoryPath {
-    return FileRepresentation(icon: "📁", color: "\u{001B}[0;34m")
-  }
-
-  if files.isExecutableFile(atPath: location.path) {
-    return FileRepresentation(icon: "⚙️", color: "\u{001B}[0;31m ")
-  }
-
-  return FileRepresentation(icon: "📃", color: "\u{001B}[0;37m")
-}
-
-func findContents(with opts: DisplayOptions) throws -> String {
-  var result = ""
-
-  let contents = try files.contentsOfDirectory(
-    at: opts.location,
-    includingPropertiesForKeys: nil,
-    options: opts.all ? [] : [.skipsHiddenFiles]
-  )
-
-  for url in contents {
-    result += try getFileAttributes(url, with: opts)
-  }
-
-  if opts.recurse {
-    if !opts.oneLine {
-      result += "\n"
+  static func determineType(_ location: URL) -> FileRepresentation {
+    if location.hasDirectoryPath {
+      return FileRepresentation(icon: "📁", color: "\u{001B}[0;34m")
     }
 
-    for url in contents {
-      if url.hasDirectoryPath {
-        result += "\n\(opts.icons ? "📁 " : "./")\(url.lastPathComponent):\n"
-        result += try findContents(
-          with: DisplayOptions(
-            location: url,
-            all: opts.all,
-            long: opts.long,
-            recurse: opts.recurse,
-            color: opts.color,
-            icons: opts.icons,
-            oneLine: opts.oneLine
-          ))
+    if fm.isExecutableFile(atPath: location.path) {
+      return FileRepresentation(icon: "⚙️ ", color: "\u{001B}[0;31m ")
+    }
+
+    return FileRepresentation(icon: "📃", color: "\u{001B}[0;37m")
+  }
+
+  static func getFileAttributes(_ location: URL, with opts: DisplayOptions) throws -> String {
+    var result = ""
+    let fileAttributes = try fm.attributesOfItem(atPath: location.path)
+    let file = determineType(location)
+
+    if opts.icons {
+      result.append(file.icon + " ")
+    }
+
+    if opts.long {
+      result.append(String(fileAttributes[.posixPermissions] as! Int) + " ")
+      result.append(fileAttributes[.ownerAccountName] as! String + " ")
+      result.append(fileAttributes[.groupOwnerAccountName] as! String + " ")
+      result.append(String(format: "%-4d", fileAttributes[.size] as! Int) + " ")
+
+      if let modificationDate = fileAttributes[.modificationDate] as? Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMM HH:mm"
+        result.append(dateFormatter.string(from: modificationDate) + " ")
       }
     }
+
+    if opts.color {
+      result.append(file.color)
+      result.append(location.lastPathComponent)
+      result.append("\u{001B}[0;0m")
+    } else {
+      result.append(location.lastPathComponent)
+    }
+
+    if opts.oneLine || opts.long {
+      result.append("\n")
+    } else {
+      result.append("  ")
+    }
+
+    return result
   }
 
-  return result
+  static func findContents(with opts: DisplayOptions) throws -> String {
+    var result = ""
+
+    let contents = try fm.contentsOfDirectory(
+      at: opts.location,
+      includingPropertiesForKeys: nil,
+      options: opts.all ? [] : [.skipsHiddenFiles]
+    )
+
+    for url in contents {
+      result += try getFileAttributes(url, with: opts)
+    }
+
+    if opts.recurse {
+      if !opts.oneLine {
+        result += "\n"
+      }
+
+      for url in contents {
+        if url.hasDirectoryPath {
+          result += "\n\(opts.icons ? "📁 " : "./")\(url.lastPathComponent):\n"
+          result += try findContents(
+            with: DisplayOptions(
+              location: url,
+              all: opts.all,
+              long: opts.long,
+              recurse: opts.recurse,
+              color: opts.color,
+              icons: opts.icons,
+              oneLine: opts.oneLine
+            ))
+        }
+      }
+    }
+
+    return result
+  }
 }
 
 @main
 struct SwiftList: ParsableCommand {
   @Flag(name: .shortAndLong, help: "Display all files, including hidden.")
   var all = false
-  @Flag(name: .shortAndLong, help: "Display extended details and attributes.")
+  @Flag(name: .shortAndLong, help: "Display file attributes, one file per line")
   var long = false
   @Flag(name: .shortAndLong, help: "Recurse into directories.")
   var recurse = false
   @Flag(name: .shortAndLong, help: "Colorize the output.")
   var color = false
-  @Flag(name: .shortAndLong, help: "Display each file on its own line.")
-  var oneLine = false
   @Flag(name: .shortAndLong, help: "Display icons denoting file type.")
   var icons = false
   @Argument(help: "List files at path, omit for current directory.")
   var path: String?
+  @Flag(name: .shortAndLong, help: "Display each file on its own line.")
+  var oneLine = false
 
   func run() throws {
-    let location = URL(fileURLWithPath: path ?? files.currentDirectoryPath)
-    let result = try findContents(
-      with: DisplayOptions(
-        location: location,
-        all: all,
-        long: long,
-        recurse: recurse,
-        color: color,
-        icons: icons,
-        oneLine: oneLine
-      ))
-
-    print(result)
+    print(
+      try FileManagerHelper.findContents(
+        with: DisplayOptions(
+          location: URL(fileURLWithPath: path ?? FileManagerHelper.fm.currentDirectoryPath),
+          all: all,
+          long: long,
+          recurse: recurse,
+          color: color,
+          icons: icons,
+          oneLine: oneLine
+        )
+      )
+    )
   }
 }
