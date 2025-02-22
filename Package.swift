c7b405553ef6b77cb22418929533e6f240014866
Mac <maclong9@icloud.com>
2025-02-22 16:01:11 +0000

    fix: update to fix issues with swift concurrency

--- Package.swift
+++ Package.swift
@@ -4,25 +4,16 @@ import PackageDescription
 let package = Package(
   name: "sls",
   products: [
-    .executable(
-      name: "sls",
-      targets: ["sls"]
-    )
+    .executable(name: "sls", targets: ["sls"])
   ],
   dependencies: [
-    .package(
-      url: "https://github.com/apple/swift-argument-parser",
-      from: "1.0.0"
-    )
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")
   ],
   targets: [
     .executableTarget(
       name: "sls",
       dependencies: [
-        .product(
-          name: "ArgumentParser",
-          package: "swift-argument-parser"
-        )
+        .product(name: "ArgumentParser", package: "swift-argument-parser")
       ]
     ),
     .testTarget(
